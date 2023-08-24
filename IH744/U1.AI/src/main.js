6f34dd8f67433e7094447934c1f7735f235b2970
FO <fo.edu@outlook.com>
2023-08-24 16:45:33 -0600

    A3

--- IH744/U1.AI/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/index.css';
-import router from './router/route';  // Importando el router
-
-const app = createApp(App);
-
-app.use(router);  // Usando el router
-app.mount('#app');
