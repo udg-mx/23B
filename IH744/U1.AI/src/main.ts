ce2aa919368caea1b4f8e6bdaa2fd1a787018220
FO <fo.edu@outlook.com>
2023-08-25 22:43:46 -0600

    main

--- IH744/U1.AI/src/main.ts
+++ IH744/U1.AI/src/main.ts
@@ -5,9 +5,16 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faRefresh, faPlus, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 const app = createApp(App)
 
+
+library.add(faRefresh, faPlus, faEdit, faTrash);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(createPinia())
 app.use(router)
 
