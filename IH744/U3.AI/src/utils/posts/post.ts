4a6ae81305db8157284aa8d1eb30eb747e8e186d
FO <fo.edu@outlook.com>
2023-08-27 23:19:36 -0600

    U3AI

---
+++ IH744/U3.AI/src/utils/posts/post.ts
@@ -0,0 +1,21 @@
+export interface Post {
+    id: number;
+    imageUrl: string;
+    title: string;
+    description: string;
+    body: string;
+    date: string;
+    author: string;
+}
+
+export function emptyPost(): Post {
+    return {
+        id: 0,
+        imageUrl: '',
+        title: '',
+        description: '',
+        body: '',
+        date: '',
+        author: ''
+    } as Post;
+}
